8ac67a491fb067719aff07c6e91a870a1a65cd84
Sandra Roy <Sandra.D.Roy-1@ou.edu>
2019-04-15 23:17:51 -0500

    oval class

--- Oval.java
+++ Oval.java
@@ -1,4 +1,18 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
 
-public class Oval {
+public class Oval extends Shape{
+	private int diameter1;
+	private int diameter2;
 
-}
+	public Oval(Point pointUL, int d1, int d2, Color color, boolean filled)
+	{
+      super(color, filled);
+      diameter1=d1;
+      diameter2=d2;
+     
+	}
+	
+	
+	}
